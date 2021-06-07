b717fb15a05f11a85056d5e53bd6c5b2b2eef9cc
정성훈 <52942411+jsh0128@users.noreply.github.com>
2021-06-07 19:39:36 +0900

    UPDATE:post 카테고리 검색의 이름 및 카테고리 리턴

--- backend/src/api/v1/Category/Category.ctrl/SearchPostCategory.ts
+++ backend/src/api/v1/Category/Category.ctrl/SearchPostCategory.ts
@@ -3,16 +3,17 @@ import { getRepository, Repository } from "typeorm";
 import { Category } from "../../../../entity/Category";
 import { Post } from "../../../../entity/Post";
 import { PostCategory } from "../../../../entity/PostCategory";
+import User from "../../../../entity/User";
 import { handleResponse } from "../../../../lib/handleResponse";
 
 export default async (request: Request, response: Response) => {
   try {
     const { category } = request.query;
-    console.log(category);
     const categoryRepository: Repository<Category> = getRepository(Category);
     const postCategoryRepository: Repository<PostCategory> =
       getRepository(PostCategory);
     const postRepository: Repository<Post> = getRepository(Post);
+    const userRepository: Repository<User> = getRepository(User);
 
     const checkCategory = await categoryRepository.findOne({
       where: { idx: category },
@@ -33,6 +34,29 @@ export default async (request: Request, response: Response) => {
       data.push(
         await postRepository.findOne({ where: { idx: posts[i].fk_post_idx } })
       );
+    }
+
+    const postCategories = await postCategoryRepository.find({
+      select: ["idx", "fk_post_idx", "fk_category_idx"],
+      order: { idx: "DESC" },
+    });
+
+    for (let i in data) {
+      const categories = [];
+      const userName = await userRepository.findOne({
+        email: data[i].fk_user_email,
+      });
+      for (let j in postCategories) {
+        if (postCategories[j].fk_post_idx === data[i].idx) {
+          categories.push(
+            await categoryRepository.findOne({
+              where: { idx: postCategories[j].fk_category_idx },
+            })
+          );
+        }
+      }
+      data[i].categories = categories;
+      data[i].user_name = userName.name;
     }
 
     handleResponse(response, 200, "조회 성공", data);
