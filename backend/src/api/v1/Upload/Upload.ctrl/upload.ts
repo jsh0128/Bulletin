56a4395c483e40f400a3b3c23c8244060299781d
정성훈 <52942411+jsh0128@users.noreply.github.com>
2021-06-14 15:34:10 +0900

    FIX:사진 없을때 처리

--- backend/src/api/v1/Upload/Upload.ctrl/upload.ts
+++ backend/src/api/v1/Upload/Upload.ctrl/upload.ts
@@ -4,15 +4,18 @@ import { handleResponse } from "../../../../lib/handleResponse";
 
 export default async (request: Request, response: Response) => {
   try {
-    console.log("upload.ts");
     const requestFiles = request.files;
     let files: string[] = [];
+
+    if (!requestFiles.length) {
+      console.log("파일 업로드 실패");
+      return handleResponse(response, 404, "사진을 전송해주세요");
+    }
 
     for (let i in requestFiles) {
       files.push(createURL(requestFiles[i].filename));
     }
 
-    console.log(files);
     console.log("파일 업로드 성공");
     handleResponse(response, 200, "파일 업로드 성공", { files });
     return;
