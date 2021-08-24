2fa90104166134746b69bf3af6dcc2af8927da9f
정성훈 <52942411+jsh0128@users.noreply.github.com>
2021-08-24 09:24:07 +0900

    ADD: meta tag

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -7,10 +7,15 @@ import React from "react";
 import ErrorHandling from "lib/ErrorHandling";
 import "react-toastify/dist/ReactToastify.css";
 import { ThemeProvider } from "styled-components";
+import Head from "next/head";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Blash</title>
+      </Head>
       <ThemeProvider theme={theme}>
         <Center>
           <DefaultTemplate>
